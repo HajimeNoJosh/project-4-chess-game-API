ca84fed8f725c3c4d826b5c3c329b5ab22e1468f
HajimeNoJosh <joshuah.m.martin@gmail.com>
2019-12-02 10:40:18 -0500

    Add resource
    Added coordinate resource for chess client

---
+++ app/models/coordinate.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const coordinateSchema = new mongoose.Schema({
+  coords: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Coordinate', coordinateSchema)
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 
 // require route files
 const exampleRoutes = require('./app/routes/example_routes')
+const coordinateRoutes = require('./app/routes/coordinate_routes')
 const userRoutes = require('./app/routes/user_routes')
 
 // require middleware
@@ -64,6 +65,7 @@ app.use(requestLogger)
 
 // register route files
 app.use(exampleRoutes)
+app.use(coordinateRoutes)
 app.use(userRoutes)
 
 // register error handling middleware
